0019ec64200f16e3e4a0175b80d7292706fdcc5d
ajahsuleiman@gmail.com <111079800+Mmmustapha42@users.noreply.github.com>
2024-12-20 21:17:40 +0100

    commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,4 @@
 
-import { Box, IconButton, Tooltip } from '@mui/material';
-import ShowChartIcon from '@mui/icons-material/ShowChart';
-import ViewQuiltIcon from '@mui/icons-material/ViewQuilt';
-import AddIcon from '@mui/icons-material/Add';
-import RefreshIcon from '@mui/icons-material/Refresh';
-import GroupsIcon from '@mui/icons-material/Groups';
-import DeleteIcon from '@mui/icons-material/Delete';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import FilterListIcon from '@mui/icons-material/FilterList';
-import ViewColumnIcon from '@mui/icons-material/ViewColumn';
-
 const Navbar = () => {
   return (
     <div className=""></div>
